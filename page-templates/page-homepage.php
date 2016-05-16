0ee11dea580f6a481d216ae92c06a9006f099bc1
Kyle Turco <kyleturco@gmail.com>
2016-05-16 00:59:52 -0500

    feat(Services): add services template, update homepage

--- page-templates/page-homepage.php
+++ page-templates/page-homepage.php
@@ -10,38 +10,30 @@ Template Name: Home Page
   <div id="home-hero-section">
     <div class="container">
       <div id="inner-content">
-        <h1 class="text-white">Mediation,</h1>
-        <h1 class="text-white">Conflict Management</h1>
-        <p class="text-white bonus-line"><i>and</i></p>
-        <h1 class="text-white">Conflict Coaching</h1>
-        <h3 class="text-white">Qualified help achieving the Best outcomes for life's conflicts</h3>
+        <h1 class="text-white text-center">Mediation, Conflict Management</h1>
+        <p class="text-white text-center bonus-line"><i>and</i></p>
+        <h1 class="text-white text-center">Conflict Coaching</h1>
+        <h3 class="text-white text-center">Qualified help achieving the Best outcomes for life's conflicts</h3>
+        <div class="btn-primary home-hero-btn"><a href="get_template_directory_uri(); ?>/services">View Services</a></div>
+
+        </div>
       </div>
     </div>
   </div>
-  <div id="services-section">
+  <div class="about-section-homepage">
     <div class="container">
-      <h2>Dispute Resolution / Conflict Management Services Offered:</h2>
-      <div class="col-md-6 no-left-pad">
-        <div class="service-item">
-          <h4 class="text-teal">Mediation</h4>
-          <p><i class="fa fa-caret-right"></i> Civil, Family, Elder Care, Healthcare, Dispute Resolution</p>
+      <div id="inner-content">
+        <div class="col-md-3">
+          <img class="about-photo" src="<?php echo get_template_directory_uri(); ?>/library/images/kimberly-best-headshot.jpg">
         </div>
-        <div class="service-item">
-          <h4 class="text-teal">Facilitation of Group Discussions</h4>
-        </div>
-        <div class="service-item">
-          <h4 class="text-teal">Collaborative Problem Solving</h4>
-          <p><i class="fa fa-caret-right"></i> Family, Marriage, Relationships, Eldercare, Healthcare</p>
-        </div>
-      </div>
-      <div class="col-md-6 no-left-pad">
-        <div class="service-item">
-          <h4 class="text-teal">Conflict Coaching</h4>
-          <p><i class="fa fa-caret-right"></i> Effective Oral and Written Communication</p>
-          <p><i class="fa fa-caret-right"></i> Premarital planning Dealing with difficult people</p>
-        </div>
-        <div class="service-item">
-          <h4 class="text-teal">Conflict Seminars and Training</h4>
+        <div class="col-md-9">
+          <h2 class="text-white">About Kimberly Best, RN, MA</h2>
+          <p class="text-white">
+            I understand how challenging difficult life situations can be and the importance of
+            handling them in a manner that will achieve the best possible outcome for all involved. A strong education
+            combined with personal experience in a wide range of conflict gives me both knowledge and perspective for
+            satisfying mediation, conflict management, and communication resolution.
+          </p>
         </div>
       </div>
     </div>
---
+++ page-templates/page-services.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Template Name: Services Page
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="services-section">
+  <div class="container">
+    <h2>Dispute Resolution / Conflict Management Services Offered:</h2>
+    <div class="col-md-6 no-left-pad">
+      <div class="service-item">
+        <h4 class="text-teal">Mediation</h4>
+        <p><i class="fa fa-caret-right"></i> Civil, Family, Elder Care, Healthcare, Dispute Resolution</p>
+      </div>
+      <div class="service-item">
+        <h4 class="text-teal">Facilitation of Group Discussions</h4>
+      </div>
+      <div class="service-item">
+        <h4 class="text-teal">Collaborative Problem Solving</h4>
+        <p><i class="fa fa-caret-right"></i> Family, Marriage, Relationships, Eldercare, Healthcare</p>
+      </div>
+    </div>
+    <div class="col-md-6 no-left-pad">
+      <div class="service-item">
+        <h4 class="text-teal">Conflict Coaching</h4>
+        <p><i class="fa fa-caret-right"></i> Effective Oral and Written Communication</p>
+        <p><i class="fa fa-caret-right"></i> Premarital planning Dealing with difficult people</p>
+      </div>
+      <div class="service-item">
+        <h4 class="text-teal">Conflict Seminars and Training</h4>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
