d746232e489494e8d43dc3347153f47a4d7156ab
Kyle Turco <kyleturco@gmail.com>
2016-05-17 00:40:18 -0500

    feat(Contact): add contact form plugin & styles, home template updates

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -13,24 +13,23 @@ Template Name: Conctact Page
     </div>
   </div>
 </div>
-<div id="content" class="contact-section">
+<div id="contact-section">
   <div class="container">
-    <div id="contact-info">
-      <div class="contact-item">
-        <div class="contact-circle">
-          <i class="text-white fa fa-phone"></i>
+    <div id="contact-section-content" class="inner-content">
+      <div class="col-md-5 no-left-pad">
+        <div class="icon-section">
+          <div class="icon-1 clearfix">
+            <i class="fa fa-phone-square"></i><h4 class="contact-item">615.438.6942</h4>
+          </div>
+          <div class="icon-2 clearfix">
+            <i class="fa fa-envelope"></i><a href="mailto:kim@bestconflictsolutions.com"><h4 class="contact-item">kim@bestconflictsolutions.com</h4></a>
+          </div>
         </div>
-        <h3 class="contact-listing">615.438.6942</h3>
-        <div class="clearfix"></div>
+      </div>
+      <div class="form-container col-md-7 no-left-pad">
+        <?php echo do_shortcode( '[contact-form-7 id="22" title="Contact"]' ); ?>
       </div>
       <div class="clearfix"></div>
-      <div class="contact-item">
-        <div class="contact-circle">
-          <i class="text-white fa fa-envelope-o"></i>
-        </div>
-        <h3 class="contact-listing">kim@bestconflictsolutions.com</h3>
-        <div class="clearfix"></div>
-      </div>
     </div>
   </div>
 </div>
