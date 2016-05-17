226cf14125f9590358349880a2fc3101286d4ff5
Kyle Turco <kyleturco@gmail.com>
2016-05-16 22:36:13 -0500

    feat(Home): update button styles, update links

--- page-templates/page-homepage.php
+++ page-templates/page-homepage.php
@@ -10,13 +10,13 @@ Template Name: Home Page
   <div id="home-hero-section">
     <div class="container">
       <div id="inner-content">
-        <h1 class="text-white text-center">Mediation, Conflict Management</h1>
-        <p class="text-white text-center bonus-line"><i>and</i></p>
-        <h1 class="text-white text-center">Conflict Coaching</h1>
-        <h3 class="text-white text-center">Qualified help achieving the Best outcomes for life's conflicts</h3>
-        <div class="btn-primary home-hero-btn"><a href="get_template_directory_uri(); ?>/services">View Services</a></div>
-
+        <div class="hero-section-text">
+          <h1 class="text-white text-center">Mediation, Conflict Management</h1>
+          <p class="text-white text-center bonus-line"><i>and</i></p>
+          <h1 class="text-white text-center">Conflict Coaching</h1>
+          <h3 class="text-white text-center">Qualified help achieving the Best outcomes for life's conflicts</h3>
         </div>
+        <a href="<?php echo home_url(); ?>/services"><div class="btn btn-primary home-hero-btn">View Services</div></a>
       </div>
     </div>
   </div>
@@ -27,13 +27,14 @@ Template Name: Home Page
           <img class="about-photo" src="<?php echo get_template_directory_uri(); ?>/library/images/kimberly-best-headshot.jpg">
         </div>
         <div class="col-md-9">
-          <h2 class="text-white">About Kimberly Best, RN, MA</h2>
-          <p class="text-white">
+          <h2>About Kimberly Best, RN, MA</h2>
+          <p>
             I understand how challenging difficult life situations can be and the importance of
             handling them in a manner that will achieve the best possible outcome for all involved. A strong education
             combined with personal experience in a wide range of conflict gives me both knowledge and perspective for
             satisfying mediation, conflict management, and communication resolution.
           </p>
+          <a href="<?php echo home_url(); ?>/about"><div class="btn btn-primary">More About Kim</div></a>
         </div>
       </div>
     </div>
